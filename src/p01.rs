f25a6421853c0e8cc27cde97f28098bf65a20b52
whonore <wolfhonore@gmail.com>
2020-12-01 11:38:34 -0500

    Complete problem 1 part 2

--- src/p01.rs
+++ src/p01.rs
@@ -1,24 +1,33 @@
-use std::collections::HashSet;
+fn find_sum(n: u32, tgt: u32, xs: &[u32]) -> Option<Vec<u32>> {
+    if n == 1 {
+        return if xs.contains(&tgt) {
+            Some(vec![tgt])
+        } else {
+            None
+        };
+    }
 
-fn find_sum(tgt: u32, xs: Vec<u32>) -> Option<(u32, u32)> {
-    let xs: HashSet<u32> = xs.into_iter().collect();
-    for x in &xs {
-        if *x < tgt && xs.contains(&(tgt - x)) {
-            return Some((*x, tgt - x));
+    for (i, x) in xs.iter().enumerate() {
+        if *x < tgt {
+            if let Some(mut ys) = find_sum(n - 1, tgt - x, &xs[i + 1..]) {
+                ys.push(*x);
+                return Some(ys);
+            }
         }
     }
     None
 }
 
-fn solve(xs: Vec<u32>) -> Option<u32> {
-    find_sum(2020, xs).map(|(x, y)| x * y)
+fn solve(n: u32, xs: &[u32]) -> Option<u32> {
+    find_sum(n, 2020, xs).map(|xs| xs.iter().product())
 }
 
 pub fn run() -> Result<String, String> {
     let input = include_str!("input/p01.txt");
-    let xs = input.lines().map(|x| x.parse::<u32>().unwrap()).collect();
-    let out = solve(xs).ok_or("No solution found")?;
-    Ok(out.to_string())
+    let xs: Vec<u32> = input.lines().map(|x| x.parse::<u32>().unwrap()).collect();
+    let out1 = solve(2, &xs).ok_or("No solution found")?;
+    let out2 = solve(3, &xs).ok_or("No solution found")?;
+    Ok(format!("{} {}", out1, out2))
 }
 
 #[cfg(test)]
@@ -27,6 +36,17 @@ mod tests {
 
     #[test]
     fn test01() {
-        assert_eq!(solve(vec![1721, 979, 366, 299, 675, 1456]), Some(514579));
+        assert_eq!(
+            solve(2, &vec![1721, 979, 366, 299, 675, 1456]),
+            Some(514579)
+        );
+    }
+
+    #[test]
+    fn test02() {
+        assert_eq!(
+            solve(3, &vec![1721, 979, 366, 299, 675, 1456]),
+            Some(241861950)
+        );
     }
 }
