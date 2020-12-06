8ded58a0bea155e119dbbc4cb492d0295c98e3a3
whonore <wolfhonore@gmail.com>
2020-12-06 11:46:49 -0500

    Solve problem 6 part 2

--- src/p06.rs
+++ src/p06.rs
@@ -1,17 +1,32 @@
 use std::collections::HashSet;
 
-fn solve(qs: &[&str]) -> usize {
-    let qs = qs
-        .iter()
-        .map(|q| q.chars().filter(|c| *c != '\n').collect::<HashSet<_>>());
-    qs.map(|q| q.len()).sum()
+enum Mode {
+    Any,
+    All,
+}
+use Mode::*;
+
+fn solve(qs: &[&str], mode: Mode) -> usize {
+    qs.iter()
+        .map(|q| {
+            let mut qs = q
+                .lines()
+                .map(|person| person.chars().collect::<HashSet<_>>());
+            let first = qs.next().unwrap();
+            match mode {
+                Any => qs.fold(first, |acc, q| acc.union(&q).copied().collect()),
+                All => qs.fold(first, |acc, q| acc.intersection(&q).copied().collect()),
+            }
+            .len()
+        })
+        .sum()
 }
 
 pub fn run() -> Result<String, String> {
     let input = include_str!("input/p06.txt");
     let qs: Vec<_> = input.split("\n\n").collect();
-    let out1 = solve(&qs);
-    let out2 = "";
+    let out1 = solve(&qs, Any);
+    let out2 = solve(&qs, All);
     Ok(format!("{} {}", out1, out2))
 }
 
@@ -22,20 +37,46 @@ mod tests {
     #[test]
     fn test01() {
         assert_eq!(
-            solve(&[
-                "abc",
-                "a\n\
-                 b\n\
-                 c",
-                "ab\n\
-                 ac",
-                "a\n\
-                 a\n\
-                 a\n\
-                 a",
-                "b"
-            ]),
+            solve(
+                &[
+                    "abc",
+                    "a\n\
+                     b\n\
+                     c",
+                    "ab\n\
+                     ac",
+                    "a\n\
+                     a\n\
+                     a\n\
+                     a",
+                    "b"
+                ],
+                Any
+            ),
             11
+        )
+    }
+
+    #[test]
+    fn test02() {
+        assert_eq!(
+            solve(
+                &[
+                    "abc",
+                    "a\n\
+                     b\n\
+                     c",
+                    "ab\n\
+                     ac",
+                    "a\n\
+                     a\n\
+                     a\n\
+                     a",
+                    "b"
+                ],
+                All
+            ),
+            6
         )
     }
 }
