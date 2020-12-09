a4bef4686c51846cd714831e86fed628ceae0899
whonore <wolfhonore@gmail.com>
2020-12-09 09:32:54 -0500

    Solve problem 9 part 2

--- src/p09.rs
+++ src/p09.rs
@@ -19,15 +19,35 @@ impl XMAS {
         self.data.push(x);
         ok
     }
+
+    fn find_invalid(&mut self, data: &[u64]) -> Option<u64> {
+        data.iter().find(|x| !self.process(**x)).copied()
+    }
 }
 
-fn solve(data: &[u64], width: usize) -> Result<u64, String> {
+fn solve(data: &[u64], width: usize) -> Result<(u64, u64), String> {
     let (preamble, data) = data.split_at(width);
     let mut xmas = XMAS::new(preamble);
-    data.iter()
-        .find(|x| !xmas.process(**x))
-        .copied()
-        .ok_or_else(|| "No invalid data found".into())
+    let invalid = xmas
+        .find_invalid(data)
+        .ok_or_else(|| "No invalid data found".to_string())?;
+
+    let data = [preamble, data].concat();
+    let mut sum = 0;
+    let mut start = 0;
+    for end in 0..data.len() {
+        if invalid == sum && 1 <= end - start {
+            let min = data[start..end].iter().min().unwrap();
+            let max = data[start..end].iter().max().unwrap();
+            return Ok((invalid, min + max));
+        }
+        sum += data[end];
+        while invalid < sum {
+            sum -= data[start];
+            start += 1;
+        }
+    }
+    Err("No contiguous sum found".into())
 }
 
 pub fn run() -> Result<String, String> {
@@ -36,8 +56,7 @@ pub fn run() -> Result<String, String> {
         .lines()
         .map(|x| x.parse::<u64>().unwrap())
         .collect::<Vec<_>>();
-    let out1 = solve(&data, 25)?;
-    let out2 = "";
+    let (out1, out2) = solve(&data, 25)?;
     Ok(format!("{} {}", out1, out2))
 }
 
@@ -51,6 +70,6 @@ mod tests {
             35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
             576,
         ];
-        assert_eq!(solve(&data, 5), Ok(127));
+        assert_eq!(solve(&data, 5), Ok((127, 62)));
     }
 }
