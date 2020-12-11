16f23b57a09a6d7c6c70ad5dec4f31c2f28c211f
whonore <wolfhonore@gmail.com>
2020-12-11 14:16:01 -0500

    Solve problem 11 part 2

--- src/p11.rs
+++ src/p11.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
 enum Seat {
     Floor,
     Empty,
@@ -18,6 +18,83 @@ impl Seat {
     }
 }
 
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+enum Dir {
+    NW,
+    N,
+    NE,
+    E,
+    SE,
+    S,
+    SW,
+    W,
+}
+const DIRS: [Dir; 8] = [
+    Dir::NW,
+    Dir::N,
+    Dir::NE,
+    Dir::E,
+    Dir::SE,
+    Dir::S,
+    Dir::SW,
+    Dir::W,
+];
+
+struct DirIter<'a, A> {
+    grid: &'a Vec<Vec<A>>,
+    dir: Dir,
+    row: usize,
+    col: usize,
+}
+
+impl<A: Copy> Iterator for DirIter<'_, A> {
+    type Item = A;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.dir {
+            Dir::NW => {
+                self.row = self.row.checked_sub(1)?;
+                self.col = self.col.checked_sub(1)?;
+            }
+            Dir::N => {
+                self.row = self.row.checked_sub(1)?;
+            }
+            Dir::NE => {
+                self.row = self.row.checked_sub(1)?;
+                self.col += 1;
+            }
+            Dir::E => {
+                self.col += 1;
+            }
+            Dir::SE => {
+                self.row += 1;
+                self.col += 1;
+            }
+            Dir::S => {
+                self.row += 1;
+            }
+            Dir::SW => {
+                self.row += 1;
+                self.col = self.col.checked_sub(1)?;
+            }
+            Dir::W => {
+                self.col = self.col.checked_sub(1)?;
+            }
+        };
+        self.grid
+            .get(self.row)
+            .and_then(|row| row.get(self.col))
+            .copied()
+    }
+}
+
+enum Mode {
+    Adj,
+    Visible,
+}
+use Mode::*;
+
+#[derive(PartialEq, Eq, Debug, Clone)]
 struct Grid(Vec<Vec<Seat>>);
 
 impl FromStr for Grid {
@@ -42,18 +119,37 @@ impl FromStr for Grid {
 }
 
 impl Grid {
-    fn step(&mut self) -> bool {
+    fn dir_iter(&self, dir: Dir, row: usize, col: usize) -> DirIter<Seat> {
+        DirIter {
+            grid: &self.0,
+            dir,
+            row,
+            col,
+        }
+    }
+
+    fn step(&mut self, mode: &Mode) -> bool {
         let mut flip: Vec<(usize, usize)> = vec![];
         for (r, row) in self.0.iter().enumerate() {
             for (c, seat) in row.iter().enumerate() {
-                match seat {
-                    Empty => {
+                match (seat, mode) {
+                    (Empty, Adj) => {
                         if self.count_adj(r, c) == 0 {
                             flip.push((r, c));
                         }
                     }
-                    Occupied => {
+                    (Occupied, Adj) => {
                         if 4 <= self.count_adj(r, c) {
+                            flip.push((r, c));
+                        }
+                    }
+                    (Empty, Visible) => {
+                        if self.count_vis(r, c) == 0 {
+                            flip.push((r, c));
+                        }
+                    }
+                    (Occupied, Visible) => {
+                        if 5 <= self.count_vis(r, c) {
                             flip.push((r, c));
                         }
                     }
@@ -67,34 +163,22 @@ impl Grid {
         !flip.is_empty()
     }
 
-    fn stabilize(&mut self) {
-        while self.step() {}
+    fn stabilize(&mut self, mode: &Mode) {
+        while self.step(mode) {}
     }
 
     fn count_adj(&self, r: usize, c: usize) -> usize {
-        [
-            r.checked_sub(1)
-                .and_then(|r| self.0.get(r))
-                .and_then(|row| c.checked_sub(1).and_then(|c| row.get(c))),
-            r.checked_sub(1)
-                .and_then(|r| self.0.get(r))
-                .and_then(|row| row.get(c)),
-            r.checked_sub(1)
-                .and_then(|r| self.0.get(r))
-                .and_then(|row| row.get(c + 1)),
-            self.0
-                .get(r)
-                .and_then(|row| c.checked_sub(1).and_then(|c| row.get(c))),
-            self.0.get(r).and_then(|row| row.get(c + 1)),
-            self.0
-                .get(r + 1)
-                .and_then(|row| c.checked_sub(1).and_then(|c| row.get(c))),
-            self.0.get(r + 1).and_then(|row| row.get(c)),
-            self.0.get(r + 1).and_then(|row| row.get(c + 1)),
-        ]
-        .iter()
-        .filter(|seat| seat.map(|s| *s == Occupied).unwrap_or(false))
-        .count()
+        DIRS.iter()
+            .filter_map(|dir| self.dir_iter(*dir, r, c).next())
+            .filter(|seat| *seat == Occupied)
+            .count()
+    }
+
+    fn count_vis(&self, r: usize, c: usize) -> usize {
+        DIRS.iter()
+            .filter_map(|dir| self.dir_iter(*dir, r, c).find(|seat| *seat != Floor))
+            .filter(|seat| *seat == Occupied)
+            .count()
     }
 
     fn count_occupied(&self) -> usize {
@@ -105,22 +189,55 @@ impl Grid {
     }
 }
 
-fn solve(grid: &mut Grid) -> usize {
-    grid.stabilize();
-    grid.count_occupied()
+fn solve(grid: &mut Grid) -> (usize, usize) {
+    let mut grid2 = grid.clone();
+    grid.stabilize(&Adj);
+    grid2.stabilize(&Visible);
+    (grid.count_occupied(), grid2.count_occupied())
 }
 
 pub fn run() -> Result<String, String> {
     let input = include_str!("input/p11.txt");
     let mut grid = input.parse::<Grid>()?;
-    let out1 = solve(&mut grid);
-    let out2 = "";
+    let (out1, out2) = solve(&mut grid);
     Ok(format!("{} {}", out1, out2))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_visible() {
+        let grid = ".......#.\n\
+                    ...#.....\n\
+                    .#.......\n\
+                    .........\n\
+                    ..#L....#\n\
+                    ....#....\n\
+                    .........\n\
+                    #........\n\
+                    ...#....."
+            .parse::<Grid>()
+            .unwrap();
+        assert_eq!(grid.count_vis(4, 3), 8);
+        let grid = ".............\n\
+                    .L.L.#.#.#.#.\n\
+                    ............."
+            .parse::<Grid>()
+            .unwrap();
+        assert_eq!(grid.count_vis(1, 1), 0);
+        let grid = ".##.##.\n\
+                    #.#.#.#\n\
+                    ##...##\n\
+                    ...L...\n\
+                    ##...##\n\
+                    #.#.#.#\n\
+                    .##.##."
+            .parse::<Grid>()
+            .unwrap();
+        assert_eq!(grid.count_vis(3, 3), 0);
+    }
 
     #[test]
     fn test01() {
@@ -136,6 +253,6 @@ mod tests {
                         L.LLLLL.LL"
             .parse::<Grid>()
             .unwrap();
-        assert_eq!(solve(&mut grid), 37);
+        assert_eq!(solve(&mut grid), (37, 26));
     }
 }
