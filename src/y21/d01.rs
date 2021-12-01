7da223ef50b75247cbf001da80d2d88e4570aaf8
whonore <wolfhonore@gmail.com>
2021-12-01 09:13:36 -0500

    Solve problem 1 part 1

--- src/y21/d01.rs
+++ src/y21/d01.rs
@@ -1,24 +1,27 @@
-fn part1() -> u64 {
-    todo!()
+fn part1(depths: &[u64]) -> usize {
+    depths.windows(2).filter(|xs| xs[0] < xs[1]).count()
 }
 
 fn part2() -> u64 {
-    todo!()
+    0
 }
 
 #[allow(clippy::unnecessary_wraps)]
 pub fn run() -> Result<String, String> {
-    let _input = include_str!("input/d01.txt");
-    let out1 = part1();
+    let input = include_str!("input/d01.txt");
+    let depths = input.lines().map(|d| d.parse().unwrap()).collect::<Vec<_>>();
+    let out1 = part1(&depths);
     let out2 = part2();
     Ok(format!("{} {}", out1, out2))
 }
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
-    // #[test]
-    // fn test01() {}
+    #[test]
+    fn test01() {
+        let depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(part1(&depths), 7);
+    }
 }
-
