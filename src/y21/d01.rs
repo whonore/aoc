1f6119706c865c5fde45d0271a872053db2b534d
whonore <wolfhonore@gmail.com>
2021-12-01 09:21:08 -0500

    Solve problem 1 part 2

--- src/y21/d01.rs
+++ src/y21/d01.rs
@@ -1,17 +1,27 @@
 fn part1(depths: &[u64]) -> usize {
-    depths.windows(2).filter(|xs| xs[0] < xs[1]).count()
+    depths.windows(2).filter(|ds| ds[0] < ds[1]).count()
 }
 
-fn part2() -> u64 {
-    0
+fn part2(depths: &[u64]) -> usize {
+    depths
+        .windows(3)
+        .map(|ds| ds.iter().sum())
+        .collect::<Vec<u64>>()
+        .as_slice()
+        .windows(2)
+        .filter(|ds| ds[0] < ds[1])
+        .count()
 }
 
 #[allow(clippy::unnecessary_wraps)]
 pub fn run() -> Result<String, String> {
     let input = include_str!("input/d01.txt");
-    let depths = input.lines().map(|d| d.parse().unwrap()).collect::<Vec<_>>();
+    let depths = input
+        .lines()
+        .map(|d| d.parse().unwrap())
+        .collect::<Vec<_>>();
     let out1 = part1(&depths);
-    let out2 = part2();
+    let out2 = part2(&depths);
     Ok(format!("{} {}", out1, out2))
 }
 
@@ -23,5 +33,11 @@ mod tests {
     fn test01() {
         let depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
         assert_eq!(part1(&depths), 7);
+    }
+
+    #[test]
+    fn test02() {
+        let depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(part2(&depths), 5);
     }
 }
