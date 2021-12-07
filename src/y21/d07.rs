05d90574b62371c556b761c46781326b1be24ca0
whonore <wolfhonore@gmail.com>
2021-12-07 09:31:41 -0500

    Solve problem 7 part 1

--- src/y21/d07.rs
+++ src/y21/d07.rs
@@ -1,23 +1,38 @@
-fn part1() -> u64 {
-    todo!()
+fn median(pos: &mut [u64]) -> u64 {
+    pos.sort_unstable();
+    pos[pos.len() / 2]
+}
+
+fn part1(pos: &mut [u64]) -> u64 {
+    let c = median(pos);
+    pos.iter().map(|&x| if x > c { x - c } else { c - x }).sum()
 }
 
 fn part2() -> u64 {
-    todo!()
+    0
 }
 
 #[allow(clippy::unnecessary_wraps)]
 pub fn run() -> Result<String, String> {
-    let _input = include_str!("input/d07.txt");
-    let out1 = part1();
+    let input = include_str!("input/d07.txt");
+    let pos = input
+        .trim()
+        .split(',')
+        .map(str::parse)
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|_| "Invalid position")?;
+    let out1 = part1(&mut pos.clone());
     let out2 = part2();
     Ok(format!("{} {}", out1, out2))
 }
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
-    // #[test]
-    // fn test01() {}
+    #[test]
+    fn test01() {
+        let mut pos = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+        assert_eq!(part1(&mut pos), 37);
+    }
 }
