e8cc5a731886d35f5a28ee74e745606cb88c4e72
whonore <wolfhonore@gmail.com>
2021-12-09 11:15:21 -0500

    Solve problem 9 part 2

--- src/y21/d09.rs
+++ src/y21/d09.rs
@@ -1,28 +1,75 @@
-fn part1(heights: &[Vec<u32>]) -> u64 {
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+fn neighbors(r: usize, c: usize) -> Vec<(usize, usize)> {
+    [
+        r.checked_sub(1).zip(Some(c)),
+        Some((r + 1, c)),
+        Some(r).zip(c.checked_sub(1)),
+        Some((r, c + 1)),
+    ]
+    .iter()
+    .copied()
+    .flatten()
+    .collect()
+}
+
+fn find_low(heights: &[Vec<u32>]) -> Vec<(usize, usize)> {
     heights
         .iter()
         .enumerate()
         .flat_map(|(r, row)| {
             row.iter().enumerate().filter_map(move |(c, &h)| {
-                [
-                    r.checked_sub(1).zip(Some(c)),
-                    Some((r + 1, c)),
-                    Some(r).zip(c.checked_sub(1)),
-                    Some((r, c + 1)),
-                ]
-                .iter()
-                .filter_map(|n| *n)
-                .filter_map(|(r, c)| heights.get(r).and_then(|row| row.get(c)))
-                .copied()
-                .all(|n| h < n)
-                .then(|| u64::from(h) + 1)
+                neighbors(r, c)
+                    .iter()
+                    .filter_map(|&(r, c)| heights.get(r).and_then(|row| row.get(c)))
+                    .copied()
+                    .all(|n| h < n)
+                    .then(|| (r, c))
             })
         })
+        .collect()
+}
+
+fn find_basin(heights: &[Vec<u32>], low: (usize, usize)) -> HashSet<(usize, usize)> {
+    let mut basin = [low].iter().copied().collect::<HashSet<(usize, usize)>>();
+    let ns = neighbors(low.0, low.1);
+    let mut unchecked = ns.iter().copied().collect::<HashSet<(usize, usize)>>();
+
+    while !unchecked.is_empty() {
+        let &(r, c) = unchecked.iter().next().unwrap();
+        unchecked.remove(&(r, c));
+        if heights
+            .get(r)
+            .and_then(|row| row.get(c))
+            .copied()
+            .map_or(false, |h| h != 9)
+        {
+            basin.insert((r, c));
+            for n in neighbors(r, c).iter().filter(|n| !basin.contains(n)) {
+                unchecked.insert(*n);
+            }
+        }
+    }
+    basin
+}
+
+fn part1(heights: &[Vec<u32>]) -> u64 {
+    find_low(heights)
+        .iter()
+        .map(|&(r, c)| u64::from(heights[r][c]) + 1)
         .sum()
 }
 
-fn part2() -> u64 {
-    0
+fn part2(heights: &[Vec<u32>]) -> usize {
+    let lows = find_low(heights);
+    let basins = lows.iter().map(|&low| find_basin(heights, low));
+    basins
+        .map(|basin| basin.len())
+        .sorted_by(|x, y| x.cmp(y).reverse())
+        .take(3)
+        .product()
 }
 
 #[allow(clippy::unnecessary_wraps)]
@@ -38,7 +85,7 @@ pub fn run() -> Result<String, String> {
         })
         .collect::<Vec<_>>();
     let out1 = part1(&heights);
-    let out2 = part2();
+    let out2 = part2(&heights);
     Ok(format!("{} {}", out1, out2))
 }
 
@@ -56,5 +103,17 @@ mod tests {
             vec![9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
         ];
         assert_eq!(part1(&heights), 15);
+    }
+
+    #[test]
+    fn test02() {
+        let heights = [
+            vec![2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+            vec![3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+            vec![9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+            vec![8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+            vec![9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+        ];
+        assert_eq!(part2(&heights), 1134);
     }
 }
