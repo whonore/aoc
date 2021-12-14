40d8118a2e93560f671de150a204bf00975ab8a3
whonore <wolfhonore@gmail.com>
2021-12-14 18:46:55 -0500

    Solve problem 14 part 2

--- src/y21/d14.rs
+++ src/y21/d14.rs
@@ -1,12 +1,32 @@
 use std::collections::HashMap;
+use std::ops::AddAssign;
 use std::str::FromStr;
 
 use itertools::{Itertools, MinMaxResult};
 
+type Rules = HashMap<(char, char), char>;
+
+#[derive(Debug, Clone, Default)]
+struct Counts(HashMap<char, usize>);
+
+impl AddAssign for Counts {
+    fn add_assign(&mut self, cnts: Self) {
+        cnts.0.iter().for_each(|(c, cnt)| {
+            *self.0.entry(*c).or_insert(0) += cnt;
+        });
+    }
+}
+
+impl Counts {
+    fn dec(&mut self, c: char) {
+        self.0.entry(c).and_modify(|cnt| *cnt -= 1);
+    }
+}
+
 #[derive(Debug, Clone)]
 struct Polymer {
     poly: Vec<char>,
-    rules: HashMap<(char, char), char>,
+    rules: Rules,
 }
 
 impl FromStr for Polymer {
@@ -37,90 +57,67 @@ impl FromStr for Polymer {
 }
 
 impl Polymer {
-    fn apply(&mut self) {
-        self.poly
-            .iter()
-            .enumerate()
-            .tuple_windows()
-            .filter_map(|((_, c1), (n, c2))| self.rules.get(&(*c1, *c2)).map(|insert| (n, *insert)))
-            .collect::<Vec<_>>()
-            .iter()
-            .enumerate()
-            .for_each(|(idx, (n, insert))| {
-                self.poly.insert(n + idx, *insert);
-            });
+    fn count_pair(
+        &self,
+        cache: &mut HashMap<((char, char), u64), Counts>,
+        pair: (char, char),
+        depth: u64,
+    ) -> Counts {
+        cache.get(&(pair, depth)).cloned().unwrap_or_else(|| {
+            let cnts = if depth == 0 || !self.rules.contains_key(&pair) {
+                Counts([pair.0, pair.1].iter().copied().counts())
+            } else {
+                let insert = *self.rules.get(&pair).unwrap();
+                let mut cnts = Counts::default();
+                cnts += self.count_pair(cache, (pair.0, insert), depth - 1);
+                cnts += self.count_pair(cache, (insert, pair.1), depth - 1);
+                cnts.dec(insert);
+                cnts
+            };
+            cache.insert((pair, depth), cnts.clone());
+            cnts
+        })
     }
 }
 
-fn part1(mut poly: Polymer) -> Result<usize, String> {
-    for _ in 0..10 {
-        poly.apply();
-    }
-    if let MinMaxResult::MinMax(min, max) = poly.poly.iter().counts().values().minmax() {
+fn run_poly(poly: &Polymer, n: u64) -> Result<usize, String> {
+    let mut cache = HashMap::new();
+    let counts = poly
+        .poly
+        .iter()
+        .tuple_windows()
+        .fold(Counts::default(), |mut cnts, (c1, c2)| {
+            cnts += poly.count_pair(&mut cache, (*c1, *c2), n);
+            cnts.dec(*c1);
+            cnts
+        });
+    if let MinMaxResult::MinMax(min, max) = counts.0.values().minmax() {
         Ok(max - min)
     } else {
         Err("Couldn't find distinct min and max".into())
     }
 }
 
-fn part2() -> u64 {
-    0
+fn part1(poly: &Polymer) -> Result<usize, String> {
+    run_poly(poly, 10)
+}
+
+fn part2(poly: &Polymer) -> Result<usize, String> {
+    run_poly(poly, 40)
 }
 
 #[allow(clippy::unnecessary_wraps)]
 pub fn run() -> Result<String, String> {
     let input = include_str!("input/d14.txt");
-    let poly = input.parse()?;
-    let out1 = part1(poly)?;
-    let out2 = part2();
+    let poly = input.parse::<Polymer>()?;
+    let out1 = part1(&poly)?;
+    let out2 = part2(&poly)?;
     Ok(format!("{} {}", out1, out2))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_apply_rules() {
-        let mut poly = Polymer {
-            poly: vec!['N', 'N', 'C', 'B'],
-            rules: [
-                (('C', 'H'), 'B'),
-                (('H', 'H'), 'N'),
-                (('C', 'B'), 'H'),
-                (('N', 'H'), 'C'),
-                (('H', 'B'), 'C'),
-                (('H', 'C'), 'B'),
-                (('H', 'N'), 'C'),
-                (('N', 'N'), 'C'),
-                (('B', 'H'), 'H'),
-                (('N', 'C'), 'B'),
-                (('N', 'B'), 'B'),
-                (('B', 'N'), 'B'),
-                (('B', 'B'), 'N'),
-                (('B', 'C'), 'B'),
-                (('C', 'C'), 'N'),
-                (('C', 'N'), 'C'),
-            ]
-            .iter()
-            .copied()
-            .collect(),
-        };
-        poly.apply();
-        assert_eq!(poly.poly.iter().collect::<String>(), "NCNBCHB");
-        poly.apply();
-        assert_eq!(poly.poly.iter().collect::<String>(), "NBCCNBBBCBHCB");
-        poly.apply();
-        assert_eq!(
-            poly.poly.iter().collect::<String>(),
-            "NBBBCNCCNBBNBNBBCHBHHBCHB"
-        );
-        poly.apply();
-        assert_eq!(
-            poly.poly.iter().collect::<String>(),
-            "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
-        );
-    }
 
     #[test]
     fn test01() {
@@ -148,6 +145,35 @@ mod tests {
             .copied()
             .collect(),
         };
-        assert_eq!(part1(poly), Ok(1588));
+        assert_eq!(part1(&poly), Ok(1588));
+    }
+
+    #[test]
+    fn test02() {
+        let poly = Polymer {
+            poly: vec!['N', 'N', 'C', 'B'],
+            rules: [
+                (('C', 'H'), 'B'),
+                (('H', 'H'), 'N'),
+                (('C', 'B'), 'H'),
+                (('N', 'H'), 'C'),
+                (('H', 'B'), 'C'),
+                (('H', 'C'), 'B'),
+                (('H', 'N'), 'C'),
+                (('N', 'N'), 'C'),
+                (('B', 'H'), 'H'),
+                (('N', 'C'), 'B'),
+                (('N', 'B'), 'B'),
+                (('B', 'N'), 'B'),
+                (('B', 'B'), 'N'),
+                (('B', 'C'), 'B'),
+                (('C', 'C'), 'N'),
+                (('C', 'N'), 'C'),
+            ]
+            .iter()
+            .copied()
+            .collect(),
+        };
+        assert_eq!(part2(&poly), Ok(2_188_189_693_529));
     }
 }
