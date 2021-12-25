9c49aa5c0ff458fd3aca8c0c604cef25c55df33e
whonore <wolfhonore@gmail.com>
2021-12-25 15:12:55 -0500

    Solve problem 21 part 2

--- src/y21/d21.rs
+++ src/y21/d21.rs
@@ -1,24 +1,104 @@
-fn take3<A>(it: &mut impl Iterator<Item = A>) -> [A; 3] {
-    [it.next().unwrap(), it.next().unwrap(), it.next().unwrap()]
+use std::collections::HashMap;
+use std::iter;
+
+use itertools::Itertools;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Game {
+    positions: [u64; 2],
+    scores: [u64; 2],
+    max_score: u64,
+}
+
+impl Game {
+    const fn new(max_score: u64, p1: u64, p2: u64) -> Self {
+        Self {
+            positions: [p1 - 1, p2 - 1],
+            scores: [0; 2],
+            max_score,
+        }
+    }
+
+    fn add(&mut self, player: usize, roll: u64) {
+        self.positions[player] = (self.positions[player] + roll) % 10;
+        self.scores[player] += self.positions[player] + 1;
+    }
+
+    fn won(&self) -> bool {
+        self.scores.iter().any(|&score| score >= self.max_score)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Games(HashMap<Game, u64>);
+
+impl Games {
+    fn new(max_score: u64, p1: u64, p2: u64) -> Self {
+        Self(
+            [(Game::new(max_score, p1, p2), 1)]
+                .iter()
+                .copied()
+                .collect(),
+        )
+    }
+
+    fn add(&mut self, player: usize, rolls: &[u64]) {
+        let mut new = HashMap::new();
+        for (game, cnt) in &self.0 {
+            if game.won() {
+                *new.entry(*game).or_insert(0) += cnt;
+            } else {
+                for roll in rolls {
+                    let mut game = *game;
+                    game.add(player, *roll);
+                    *new.entry(game).or_insert(0) += cnt;
+                }
+            }
+        }
+        self.0 = new;
+    }
+
+    fn unfinished(&self) -> bool {
+        !self.0.keys().all(Game::won)
+    }
+
+    fn wins(&self, player: usize) -> u64 {
+        self.0
+            .iter()
+            .filter_map(|(game, cnt)| {
+                (game.scores[player] > game.scores[(player + 1) % 2]).then(|| cnt)
+            })
+            .sum()
+    }
 }
 
 fn part1(p1: u64, p2: u64) -> u64 {
-    let mut dice = (1..=100).cycle();
-    let mut pos = [p1 - 1, p2 - 1];
-    let mut scores = [0, 0];
+    let dice = (1..=100).cycle().chunks(3);
+    let mut dice = dice.into_iter();
+    let mut game = Game::new(1000, p1, p2);
     let mut player = 0;
     let mut rolls = 0;
-    while scores.iter().all(|&score| score < 1000) {
-        pos[player] = (pos[player] + take3(&mut dice).iter().sum::<u64>()) % 10;
-        scores[player] += pos[player] + 1;
+    while !game.won() {
+        game.add(player, dice.next().unwrap().sum::<u64>());
         player = (player + 1) % 2;
         rolls += 3;
     }
-    scores.iter().min().unwrap() * rolls
+    game.scores.iter().min().unwrap() * rolls
 }
 
-fn part2() -> u64 {
-    0
+fn part2(p1: u64, p2: u64) -> [u64; 2] {
+    let mut games = Games::new(21, p1, p2);
+    let mut player = 0;
+    let rolls = iter::repeat(1..=3)
+        .take(3)
+        .multi_cartesian_product()
+        .map(|xs| xs.iter().sum())
+        .collect::<Vec<_>>();
+    while games.unfinished() {
+        games.add(player, &rolls);
+        player = (player + 1) % 2;
+    }
+    [games.wins(0), games.wins(1)]
 }
 
 #[allow(clippy::unnecessary_wraps)]
@@ -30,8 +110,8 @@ pub fn run() -> Result<String, String> {
         (parse(lines.next().unwrap()), parse(lines.next().unwrap()))
     };
     let out1 = part1(p1, p2);
-    let out2 = part2();
-    Ok(format!("{} {}", out1, out2))
+    let out2 = part2(p1, p2);
+    Ok(format!("{} {:?}", out1, out2))
 }
 
 #[cfg(test)]
@@ -41,5 +121,10 @@ mod tests {
     #[test]
     fn test01() {
         assert_eq!(part1(4, 8), 739785);
+    }
+
+    #[test]
+    fn test02() {
+        assert_eq!(part2(4, 8), [444356092776315, 341960390180808]);
     }
 }
